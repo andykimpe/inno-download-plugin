cfb82b16b935501d73d223b458127c195f5b0f0c
mitrich <mitrich.k@gmail.com>
2015-01-11 21:03:21 +0300

    FtpDir class

---
+++ idp/ftpdir.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <set>
+#include "tstring.h"
+
+class FtpDir
+{
+public:
+    FtpDir(tstring u, tstring m, tstring d, bool r, tstring comp = _T(""));
+    bool selected(set<tstring> comp);
+
+    tstring      url;
+    tstring      mask;
+    tstring      destdir;
+    bool         recursive;
+    set<tstring> components;
+    tstring      compstr;
+};
